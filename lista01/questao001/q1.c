2fd5b62d298326bb901655e937cb3445e8dfc69a
Bianca <biancamartinst25@gmail.com>
2025-08-26 23:24:32 -0400

    cod da q1e2

---
+++ lista01/questao001/q1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+//declaracao de variaveis
+int n1,n2,soma;
+//entrada de dados
+printf("digite um numero\n");
+scanf("%i",&n1);
+printf("digite o segundo numero\n");
+scanf("%i",&n2);
+//processamento e saida
+soma=n1+n2;
+printf("A soma e: %i\n", soma);
+return 0;
+}
--- lista01/questao002/main.c
+++ lista01/questao002/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    // TODO: resolução da questão 002
-    // Use entradas/saídas padrão. Ex.: scanf/printf
-    return 0;
+    //declaraçao de variaveis
+    float n1,n2,resultado=0;
+    printf("Digite o primeiro valor: ");
+    scanf("%f",&n1);
+    printf("Digite o segundo valor: ");
+    scanf("%f",&n2);
+    //processamento e saida
+    resultado=n1+n2;
+    printf("A diferenca e: %f\n",resultado);
+    return 0 ;
 }
